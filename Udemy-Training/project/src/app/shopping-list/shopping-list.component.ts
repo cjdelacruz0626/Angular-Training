9429b5f6f2c68ab602814486bb4296bebcf95f3f
De La Cruz <c.o.dela.cruz@accenture.com>
2021-10-29 11:55:00 +0800

    Reset The Project Worked on Directives

--- Udemy-Training/project/src/app/shopping-list/shopping-list.component.ts
+++ Udemy-Training/project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -7,15 +8,17 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
   ingredients: Ingredient[] = [
-    new Ingredient('Pork Meat', 6),
-    new Ingredient('Chicken Meat', 3),
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
   ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
